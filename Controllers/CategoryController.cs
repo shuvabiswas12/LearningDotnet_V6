a6c5effb614cbbcba828924fdb96c43367de9bac
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-03-15 00:21:42 +0600

    Created category form, Added client and server side validation.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,8 +14,27 @@ namespace LearningDotnet_V6.Controllers
 
         public IActionResult Index()
         {
+            // We are not used List<> here because List<> returns null if object is empty.
+            // But IEnumerable<> does not returns empty or null objects.
             IEnumerable<Category> categories = _context.Categories;
             return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category) {
+            // Server side validation using "ModelState.IsValid"
+           if (ModelState.IsValid) {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View();
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LearningDotnet_V6.Models
 {
@@ -6,8 +7,11 @@ namespace LearningDotnet_V6.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public String Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
