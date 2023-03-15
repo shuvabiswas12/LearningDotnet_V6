36749045a514115cef27ce19640564d544107f5c
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-03-15 13:59:15 +0600

    Added Edit & Delete feature.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using LearningDotnet_V6.Data;
 using LearningDotnet_V6.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LearningDotnet_V6.Controllers
 {
@@ -27,14 +28,64 @@ namespace LearningDotnet_V6.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category) {
+        public IActionResult Create(Category category)
+        {
             // Server side validation using "ModelState.IsValid"
-           if (ModelState.IsValid) {
+            if (ModelState.IsValid)
+            {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
             }
-           return View();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id.Equals(null) || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid && category != null)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeleteCategory(int id)
+        {
+            if (id.Equals(null) || id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var category = _context.Categories.Find(id);
+                if (category == null) { return NotFound(); }
+                else
+                {
+                    _context.Categories.Remove(category);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                };
+            }
         }
     }
 }
